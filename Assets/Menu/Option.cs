2f51e89d5d6d1568435c09162b5e17c9cbd717e6
Matheo <matheo.beaunez@epitech.eu>
2022-05-21 16:00:16 +0200

    [ADD] button settings

---
+++ Assets/Menu/Option.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Option : MonoBehaviour
+{
+    public void QuitSettings()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+            SceneManager.LoadScene("Menu");
+    }
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+}
